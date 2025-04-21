9fdf23429b607a885d4f083884bab485976bcc27
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2025-04-20 21:28:21 -0400

    add column sampling to sampler

--- src/sampler.py
+++ src/sampler.py
@@ -6,8 +6,12 @@ def main():
         description="Samples sequences from a FASTA file")
     parser.add_argument("-n",
                         type=int, 
-                        help="the number of sequences to sample",
-                        required=True)
+                        help="the number of sequences to sample "
+                        "(default: sample all)")
+    parser.add_argument("-c",
+                        type=int, 
+                        help="the number of columns to sample "
+                        "(default: sample all)")
     parser.add_argument("input_file",
                         type=argparse.FileType("r"),
                         help="the FASTA file to sample from")
@@ -16,17 +20,30 @@ def main():
                         help="the file to output the samples to")
     args = parser.parse_args()
 
-    if args.n <= 0:
-        raise ValueError("Must sample at least 1 sequence")
     fasta_data = args.input_file.read().strip().split('\n')
     num_sequences = len(fasta_data) // 2
+    seq_length = len(fasta_data[1])
+    if args.n is None:
+        args.n = num_sequences
+    if args.c is None:
+        args.c = seq_length
+    if args.n <= 0:
+        raise ValueError("Must sample at least 1 sequence")
     if args.n > num_sequences:
         raise ValueError(f"Cannot sample {args.n} sequences from a file with "
                          f"{num_sequences} sequences")
-    samples = random.sample(range(num_sequences), args.n)
-    for i in samples:
+    if args.c <= 0:
+        raise ValueError("Must sample at least 1 column")
+    if args.c > seq_length:
+        raise ValueError(f"Cannot sample {args.c} columns from sequences with "
+                         f"length {seq_length}")
+    seq_samples = random.sample(range(num_sequences), args.n)
+    column_samples = sorted(random.sample(range(seq_length), args.c))
+    for i in seq_samples:
         args.output_file.write(fasta_data[2*i] + '\n')
-        args.output_file.write(fasta_data[2*i+1] + '\n')
+        seq = fasta_data[2*i+1]
+        sampled_seq = ''.join(seq[j] for j in column_samples)
+        args.output_file.write(sampled_seq + '\n')
 
 if __name__ == "__main__":
     main()
